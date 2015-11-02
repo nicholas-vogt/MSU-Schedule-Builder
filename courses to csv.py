ec8be974171cc9eb3843cb3b8a4833a7ac63e276
__nev__ <nicholas-edward@users.noreply.github.com>
2015-11-03 04:38:10 +1100

    Update courses to csv.py

--- courses to csv.py
+++ courses to csv.py
@@ -1,34 +1,9 @@
 ''' 
-Desired Result for End-Users
-----------------------------
-I want this easily accessible online. The webpage should be mobile-friendly.
-According to the following queries, I want the page to display the results, 
-followed by optional excel and pdf downloads.
-    1.   Up to three degree programs.
-    2.   Up to three minor fields of study.
-    3.   Current year of study.
-    4.   Willingness to study over summer. 
+Scrape relevant text from MSU's Registrar and put into csv format with a tab delimiter.
 
-More Specs
-----------
-    1.  Limit third-party dependencies. The last thing I want is 90-percent 
-        of the code becoming useless when some import updates.
-    2.  Keep it local. It's time consuming to keep pulling source code from
-        the source. Do it once, then pull from file.
-
-Gathering Data ( * : Incomplete )
----------------------------------
-    1. Save the source code locally for each department. 
- *  2. Pull the following data from the source code. 
-        a. Course Code (This included a dept prefix)
-        b. Course Title
-        c. Prerequisites
-
-Fortunately, everything we need is online at the MSU Registrar's Office.
-
-    URL = 'https://reg.msu.edu/Courses/Request.aspx?'
+ToDo: Sort info properly by column. Some courses have more parameters than others.
 '''
-from sys import exit
+exitfrom sys import exit
 import html.parser
 import re
 
@@ -61,8 +36,7 @@ exit()
 line = line.split('\t')
 a = line[0::2]
 b = line[1::2]
-zipped = zip(a,b)
+zipped = zip(a, b)
 for i in zipped:
     print(i)
-        
     # print(line)
